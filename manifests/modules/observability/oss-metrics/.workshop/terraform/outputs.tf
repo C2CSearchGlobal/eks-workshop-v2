e00a372e7538d4da23c8ebb36380e4eff5ad5597
Hyeonho Son[Harry] <harry.son@navercorp.com>
2024-07-16 16:00:15 -0700

    Add pods logging modules to oss metric module

--- manifests/modules/observability/oss-metrics/.workshop/terraform/outputs.tf
+++ manifests/modules/observability/oss-metrics/.workshop/terraform/outputs.tf
@@ -3,5 +3,6 @@ output "environment_variables" {
   value = {
     AMP_ENDPOINT  = aws_prometheus_workspace.this.prometheus_endpoint
     ADOT_IAM_ROLE = module.iam_assumable_role_adot.iam_role_arn
+    CLOUDWATCH_LOG_GROUP_NAME = local.cw_log_group_name
   }
 }
